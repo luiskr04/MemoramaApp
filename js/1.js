14abff50d4e0e4ae067118456558c572c82fca9d
luiskr04 <luis.angel.dh@gmail.com>
2018-10-24 18:09:03 -0500

    memorama funcionando, quitar alertas, agregar imagenes

--- js/1.js
+++ js/1.js
@@ -1,21 +1,23 @@
 $(function(){
+
 	vectorImagen = new Array(33);
 	var valor1 = 0;
 	var valor2 = 0;
 	var turno = 0;
+	
 
 	/*Funcion aleatorio*/
 	function CargarAleatorio(){
 
 		//Carga de imagenes 1-16 posiciones.
-		for(int i=1; i < 17 ; i++){
+		for( i=1; i < 17 ; i++){
 			vectorImagen[i] = i+".jpg";
 		}
 
 		//Carga de imagenes 16-32 posiciones.
 		var k=1;
 		for(i=17; i < 33 ; i++){
-			vectorImagen[i] = k+"";
+			vectorImagen[i] = k+".jpg";
 			k++;
 		}
 
@@ -27,9 +29,11 @@ $(function(){
 			vectorImagen[i] = vectorImagen[aleatorio];
 			vectorImagen[aleatorio] = aux;
 		}
+
 		for(i=1;i < 33 ; i++){
 			$("#"+i+"a").attr("src","img/"+vectorImagen[i]);
 		}
+		alert('Imagenes Cargadas...');
 	}
 
 	/*Funcion ocultar*/
@@ -42,36 +46,57 @@ $(function(){
 	/* Llamamos a la Funcion ocultar*/
 	$("#ocultar").on("click",function(){
 		ocultar();
+		
 	})
+
+	$("#cargar").on("click",function(){
+		CargarAleatorio();
+	})
+
+	
 
 	/*Funcion evento*/
 	function evento(identificador){
+
+		alert(turno);
+
 		if(turno == 0)
 		{
 			valor1 = identificador;
-			turno = 1;
 			$("#"+valor1+"a").attr("src","img/"+vectorImagen[valor1]);
-		}else
+			turno = 1;
+			alert('valor uno asignado '+turno);
+		}
+		else
 		{
 			valor2 = identificador;
-			turno = 0;
-			$("#"+valor2+"a").attr("src","img/"+vectorimagen[valor2]);
 
-			if(vectorImagen[valor1] == vectorImagen[valor2])
+			turno = 0;
+			alert('se asigno valor 0 a turno');
+
+			$("#"+valor2+"a").attr("src","img/"+vectorImagen[valor2]);
+
+			alert('valor dos asignado'+vectorImagen[valor2]);
+
+			if(vectorImagen[valor2] == vectorImagen[valor1])
 			{
 				alert('Has acertado');
-			}else
+			}
+			else
 			{
 				alert('Fallaste, intentalo de nuevo.');
 				$("#"+valor1+"a").attr("src","img/0.jpg");
 				$("#"+valor2+"a").attr("src","img/0.jpg");
 			}
 		}
-	}
 
+
+	}
 	/*Llamar a la funcion evento*/
-	$(".boton").on("click",function(){
+	$(".boton").on("click", function(){
 		var identificador = $(this).data('id');
 		evento(identificador);
 	})
+
+	
 })
